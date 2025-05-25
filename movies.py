fee386cf3b98d18b90f8672c1847047a2cf291bf
ElvenBells <rubtsovaada@gmail.com>
2025-05-26 01:59:54 +0500

    create movies

--- movies.py
+++ movies.py
@@ -21,3 +21,21 @@ class MovieCollection:
         if movie.title in self._movies:
             raise ValueError(f"Фильм '{movie.title}' уже существует в коллекции")
         self._movies[movie.title] = movie
+
+    def remove_movie(self, title: str) -> None:
+        """Удаляет фильм из коллекции."""
+        if title not in self._movies:
+            raise ValueError(f"Фильм '{title}' не найден в коллекции")
+        for collection in self._collections.values():
+            if title in collection:
+                collection.remove(title)
+        del self._movies[title]
+
+
+
+if __name__ == "__main__":
+    collection = MovieCollection()
+    
+    collection.add_movie(Movie("Shrek", 2001, "Andrew Ralph Adamson", "Animation", 7.9))
+    collection.add_movie(Movie("Shrek 2", 2004, "Andrew Ralph Adamson", "Animation", 7.4))
+    collection.add_movie(Movie("The Shawshank Redemption", 1994, "Frank Darabont", "Drama", 9.3))
