8ad8e9f8a5dc52d1d116212c2fc1dd17f525b24e
ElvenBells <rubtsovaada@gmail.com>
2025-05-26 01:43:54 +0500

    movie collection class: add/remove movie from collection

--- movies.py
+++ movies.py
@@ -9,3 +9,15 @@ class Movie:
     director: str
     genres: str
     rating: float
+
+class MovieCollection:
+    """Класс для управления коллекцией фильмов."""
+    def __init__(self) -> None:
+        self._movies: Dict[str, Movie] = {}
+        self._collections: Dict[str, List[str]] = {}
+    
+    def add_movie(self, movie: Movie) -> None:
+        """Добавляет фильм в коллекцию."""
+        if movie.title in self._movies:
+            raise ValueError(f"Фильм '{movie.title}' уже существует в коллекции")
+        self._movies[movie.title] = movie
